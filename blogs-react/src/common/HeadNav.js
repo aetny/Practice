12020ae877d2b51e6a023f130e3c20c68b37b019
e <2094251034@qq.com>
2019-08-09 14:55:51 +0800

    导航栏布局调整和scss自动编译转换成css文件

--- blogs-react/src/common/HeadNav.js
+++ blogs-react/src/common/HeadNav.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
-import {Menu,Icon} from 'antd'
+import {Menu,Icon} from 'antd';
+import {Link} from 'react-router-dom';
+import 'antd/dist/antd.css';
 import './HeadNav.scss'
 
 const {SubMenu} = Menu;
@@ -11,32 +13,69 @@ export class HeadNav extends Component{
   state = {
     current:'home',
   }
+  handClick=(e)=>{
+    console.log(e);
+    this.setState({
+      current:e.key
+    })
+  }
+  handClickGlobal=()=>{
+    this.setState({
+      current:'home'
+    })
+  }
   render(){
     return (
       <div id="HeadNav">
         <div className="nav-wrap">
-          <div className="nav-logo-wrap">
-            <Icon type="global" className="nav-logo"></Icon>
+          <div className="nav-logo-wrap"
+            onClick={()=>this.handClickGlobal()}>
+              <Link to="/login/">
+                <Icon type="global" className="nav-logo"></Icon>
+              </Link>
           </div>
           <div className="nav-list-wrap">
             <Menu
               selectedKeys={[this.state.current]}
-              mode="horizontal">
+              mode="horizontal"
+              className="nav-list-menu"
+              onClick={(event)=>this.handClick(event)}>
               <Menu.Item className="" key="home">
-                <Icon type="bank" />
-                首页
+                <Link to="/login/">
+                  <Icon type="bank" />
+                  首页
+                </Link>
+                
               </Menu.Item>
-              <SubMenu key="aboutme" title={
+              <Menu.Item className="" key="articleShare">
+                <Link to="/login/Article">
+                  <Icon type="read" />
+                  文章分享
+                </Link>
+              </Menu.Item>
+              <Menu.Item className="" key="resourceShare">
+                <Link to="/login/Resource">
+                  <Icon type="read" />
+                  资源分享
+                </Link>
+              </Menu.Item>
+              <SubMenu key="aboutMe" title={
                 <span>
-                  <Icon type="user" />
+                  <Icon type="user" /> 
                   关于我
                 </span>
               }>
                 <Menu.Item key="setting1">
-                  Option 1
+                  <Link to="/login/About">
+                    <Icon type="read" />
+                    Option 1
+                </Link>
                 </Menu.Item>
                 <Menu.Item key="setting2">
-                  Option 2
+                  <Link to="/login/About">
+                    <Icon type="read" />
+                    Option 2
+                  </Link>
                 </Menu.Item>
               </SubMenu>
             </Menu>
