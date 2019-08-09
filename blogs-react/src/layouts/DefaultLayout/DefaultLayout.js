0e115e2f9a1197e780ccf409b2a8ac0332d49631
e <2094251034@qq.com>
2019-08-09 14:53:15 +0800

    规划路由

--- blogs-react/src/layouts/DefaultLayout/DefaultLayout.js
+++ blogs-react/src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,5 +1,10 @@
 import React,{Component} from 'react';
-import {HeadNav} from '../../common/HeadNav'
+import {HeadNav} from '../../common/HeadNav';
+import {Route} from 'react-router-dom';
+import {Home} from '../../routes/Home/Home';
+import {About} from '../../routes/AboutMe/AboutMe'
+import {Article} from '../../routes/ArticleShare/ArticleShare'
+import {Resource} from '../../routes/ResourceShare/ResourceShare'
 import './DefaultLayout.scss'
 export class DefaultLayout extends Component{
   render(){
@@ -9,6 +14,12 @@ export class DefaultLayout extends Component{
         Hello World
         <p className="test">1</p>
         <p>2</p>
+        <div className="content-wrap">
+          <Route path={this.props.match.url + '/'} component={Home} exact></Route>
+          <Route path={this.props.match.url + '/Resource'} component={Resource}></Route>
+          <Route path={this.props.match.url + '/Article'} component={Article}></Route>
+          <Route path={this.props.match.url + '/About'} component={About}></Route>
+        </div>
       </div>
     )
   }
--- blogs-react/src/router.js
+++ blogs-react/src/router.js
@@ -2,15 +2,15 @@ import React , {Component} from 'react';
 import {HashRouter,Route,Switch} from 'react-router-dom';
 import {DefaultLayout} from './layouts/DefaultLayout/DefaultLayout';
 import {LoginUser} from './layouts/LoginUser/LoginUser';
+
 export class RouterWrap extends Component{
   render(){
     return (
       <div id="router">
         <HashRouter>
           <Switch>
-            <Route path="/" component={DefaultLayout} exact></Route>
-            <Route path="/login" component={LoginUser}></Route>
-            <Route path="/login1" component={DefaultLayout}></Route>
+            <Route path="/" component={LoginUser} exact></Route>
+            <Route path="/login" component={DefaultLayout} ></Route>
           </Switch>
         </HashRouter>
       </div>
